3be635362f0247e582202d3f8c502ead967c630c
Jan Kostejn <jan.kostejn@thenetw.org>
2024-08-30 12:51:00 +0200

    wrong path fixed

--- modules/users.nix
+++ modules/users.nix
@@ -12,7 +12,7 @@
     {
       home-manager.useGlobalPkgs = true;
       home-manager.useUserPackages = true;
-      home-manager.users.honzakostejn = import ./home/honzakostejn;
+      home-manager.users.honzakostejn = import ../home/honzakostejn;
       home-manager.extraSpecialArgs = { inherit inputs; };
     }
   ];
