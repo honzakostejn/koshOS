8a4b0f61da3a27fc0466c6cac240edee40db340d
honzakostejn <kostejn@me.com>
2024-09-24 22:32:45 +0200

    hibernation automated

--- systems/x86_64-linux/framework/power.nix
+++ systems/x86_64-linux/framework/power.nix
@@ -1,8 +1,55 @@
 {
+  pkgs,
+  ...
+}: let
+  hibernateEnvironment = {
+    HIBERNATE_SECONDS = "180";
+    HIBERNATE_LOCK = "/var/run/autohibernate.lock";
+  };
+
+in {
+  systemd.services."awake-after-suspend-for-a-time" = {
+    description = "Sets up the suspend so that it'll wake for hibernation only if not on AC power";
+    wantedBy = [ "suspend.target" ];
+    before = [ "systemd-suspend.service" ];
+    environment = hibernateEnvironment;
+    script = ''
+      if [ $(cat /sys/class/power_supply/ACAD/online) -eq 0 ]; then
+        curtime=$(date +%s)
+        # echo "$curtime $1" >> /tmp/autohibernate.log
+        echo "$curtime" > $HIBERNATE_LOCK
+        ${pkgs.utillinux}/bin/rtcwake -m no -s $HIBERNATE_SECONDS
+      # else
+        # echo "System is on AC power, skipping wake-up scheduling for hibernation." >> /tmp/autohibernate.log
+      fi
+    '';
+    serviceConfig.Type = "simple";
+  };
+
+  systemd.services."hibernate-after-recovery" = {
+    description = "Hibernates after a suspend recovery due to timeout";
+    wantedBy = [ "suspend.target" ];
+    after = [ "systemd-suspend.service" ];
+    environment = hibernateEnvironment;
+    script = ''
+      curtime=$(date +%s)
+      sustime=$(cat $HIBERNATE_LOCK)
+      rm $HIBERNATE_LOCK
+      if [ $(($curtime - $sustime)) -ge $HIBERNATE_SECONDS ] ; then
+        systemctl hibernate
+      else
+        ${pkgs.utillinux}/bin/rtcwake -m no -s 1
+      fi
+    '';
+    serviceConfig.Type = "simple";
+  };
+
   services = {
     logind = {
       powerKey = "poweroff";
-      lidSwitch = "suspend-then-hibernate";
+      lidSwitch = "suspend";
+      # `suspend-then-hibernate` option hibernates only at 5% of remaining battery
+      # thus hibernation is automated with `HIBERNATE_SECONDS
     };
 
     power-profiles-daemon.enable = true;
