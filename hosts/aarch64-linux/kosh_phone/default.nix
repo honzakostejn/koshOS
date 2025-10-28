493745da3b5e7699d52cc42b22b8e1d8c8e133d3
honzakostejn <kostejn@me.com>
2025-10-28 22:31:17 +0100

    nix-on-droid terminal config

--- hosts/aarch64-linux/kosh_phone/default.nix
+++ hosts/aarch64-linux/kosh_phone/default.nix
@@ -10,12 +10,6 @@
     experimental-features = nix-command flakes
   '';
 
-  environment.packages = with pkgs; [
-    git
-    gh
-    vim
-  ];
-
   environment.etcBackupExtension = ".bak";
 
   # android-integration.termux-open.enable = true;
@@ -39,4 +33,28 @@
   android-integration.termux-wake-unlock.enable = true;
   android-integration.termux-reload-settings.enable = true;
   android-integration.termux-setup-storage.enable = true;
+
+  terminal.font = "${pkgs.nerd-fonts.jetbrains-mono}/share/fonts/truetype/NerdFonts/JetBrainsMono/JetBrainsMonoNerdFont-Regular.ttf";
+  terminal.colors = {
+    # https://github.com/Iamafnan/termux-tokyonight/blob/main/colorschemes/tokyonight-night.properties
+    foreground = "#c0caf5";
+    cursor = "#c0caf5";
+    background = "#1a1b26";
+    color0 = "#15161E";
+    color1 = "#f7768e";
+    color2 = "#9ece6a";
+    color3 = "#e0af68";
+    color4 = "#7aa2f7";
+    color5 = "#bb9af7";
+    color6 = "#7dcfff";
+    color7 = "#a9b1d6";
+    color8 = "#414868";
+    color9 = "#f7768e";
+    color10 = "#9ece6a";
+    color11 = "#e0af68";
+    color12 = "#7aa2f7";
+    color13 = "#bb9af7";
+    color14 = "#7dcfff";
+    color15 = "#c0caf5";
+  };
 }
--- hosts/aarch64-linux/kosh_phone/home.nix
+++ hosts/aarch64-linux/kosh_phone/home.nix
@@ -1,76 +1,21 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   imports = [
+    ../../../home/honzakostejn/programs/opencode
     ../../../home/honzakostejn/programs/nushell
     ../../../home/honzakostejn/programs/yazi
   ];
 
   home.stateVersion = "25.05";
 
-  # programs.yazi = {
-  #   enable = true;
+  home.packages = with pkgs; [
+    git
+    gh
+    vim
+  ];
 
-  #   enableNushellIntegration = true;
-  #   shellWrapperName = "y";
-
-  #   keymap = {
-  #     # https://github.com/sxyazi/yazi/blob/shipped/yazi-config/preset/keymap-default.toml
-  #     mgr.prepend_keymap = [
-  #       # hopping - remap vim motions to home row keys
-  #       {
-  #         on = [ "j" ];
-  #         run = "leave";
-  #         description = "Go back to the parent directory";
-  #       }
-  #       {
-  #         on = [ "k" ];
-  #         run = "arrow next";
-  #         description = "Move cursor down";
-  #       }
-  #       {
-  #         on = [ "l" ];
-  #         run = "arrow prev";
-  #         description = "Move cursor up";
-  #       }
-  #       {
-  #         on = [ ";" ];
-  #         run = "enter";
-  #         description = "Enter the child directory";
-  #       }
-
-  #       # navigation - remap vim motions to home row keys
-  #       {
-  #         on = [ "J" ];
-  #         run = "back";
-  #         description = "Go back to the previous directory";
-  #       }
-  #       {
-  #         on = [ ":" ];
-  #         run = "forward";
-  #         description = "Go forward to the next directory";
-  #       }
-
-  #       # seeking - remap vim motions to home row keys
-  #       {
-  #         on = [ "K" ];
-  #         run = "seek 5";
-  #         description = "Seek down 5 units in the preview";
-  #       }
-  #       {
-  #         on = [ "L" ];
-  #         run = "seek -5";
-  #         description = "Seek up 5 units in the preview";
-  #       }
-  #     ];
-  #   };
-
-  #   settings = {
-  #     mgr = {
-  #       show_hidden = true;
-  #       sort_by = "mtime";
-  #       sort_dir_first = true;
-  #       sort_reverse = true;
-  #     };
-  #   };
-  # };
+  programs.yazi = {
+    # reset keymap to default
+    keymap = { };
+  };
 }
