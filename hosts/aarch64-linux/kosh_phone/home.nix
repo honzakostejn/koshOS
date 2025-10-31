38b1d006cdc38a7ad748d38db4419281e0267c8c
honzakostejn <kostejn@me.com>
2025-10-31 09:55:27 +0100

    helix + termux config

--- hosts/aarch64-linux/kosh_phone/home.nix
+++ hosts/aarch64-linux/kosh_phone/home.nix
@@ -1,9 +1,10 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 {
   imports = [
     ../../../home/honzakostejn/programs/opencode
     ../../../home/honzakostejn/programs/nushell
     ../../../home/honzakostejn/programs/yazi
+    ../../../home/honzakostejn/programs/helix
   ];
 
   home.stateVersion = "25.05";
@@ -14,23 +15,56 @@
     vim
   ];
 
-  programs.nushell = {
-    extraEnv = ''
-      $env.PATH = ($env.PATH | split row (char esep) | prepend "/data/data/com.termux/files/usr/bin/applets" | prepend "/data/data/com.termux/files/usr/bin" | prepend ($env.HOME + "/bin"))
-    '';
+  home.sessionVariables = {
+    EDITOR = "helix";
   };
 
-  home.file."bin/opencode" = {
+  # programs.nushell = {
+  #   extraEnv = ''
+  #     $env.PATH = ($env.PATH | split row (char esep) | prepend "/data/data/com.termux/files/usr/bin/applets" | prepend "/data/data/com.termux/files/usr/bin" | prepend ($env.HOME + "/bin"))
+  #   '';
+  # };
+
+  # home.file."bin/opencode" = {
+  #   text = ''
+  #     #!/usr/bin/env bash
+  #     export PATH="/data/data/com.termux/files/usr/bin:/data/data/com.termux/files/usr/bin/applets:$PATH"
+  #     exec ${config.home.homeDirectory}/.nix-profile/bin/opencode "$@"
+  #   '';
+  #   executable = true;
+  # };
+
+  home.file.".termux/termux.properties" = {
     text = ''
-      #!/usr/bin/env bash
-      export PATH="/data/data/com.termux/files/usr/bin:/data/data/com.termux/files/usr/bin/applets:$PATH"
-      exec ${config.home.homeDirectory}/.nix-profile/bin/opencode "$@"
+      # send escape for back key
+      back-key=escape
+
+      # ctrl = volume down
+      shortcut.create-session=ctrl + t
+      shortcut.next-session=ctrl + m
     '';
-    executable = true;
   };
 
   programs.yazi = {
-    # reset keymap to default
-    keymap = { };
+    # kosh_phone has different keyboard layout than standard qwerty
+    keymap = lib.mkForce { };
+  };
+
+  programs.helix = {
+    settings = {
+      # kosh_phone has different keyboard layout than standard qwerty
+      keys = {
+        normal = lib.mkForce {
+          # yazi integration
+          C-y = [
+            ":sh rm -f /tmp/helix-yazi"
+            ":insert-output yazi %{buffer_name} --chooser-file=/tmp/helix-yazi"
+            ":insert-output echo \\x1b[?1049h\\x1b[?2004h > /dev/tty"
+            ":open %sh{cat /tmp/helix-yazi}"
+            ":redraw"
+          ];
+        };
+      };
+    };
   };
 }
