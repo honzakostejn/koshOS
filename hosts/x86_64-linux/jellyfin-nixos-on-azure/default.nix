91dbde32f9ef22104489ef5882357777e925b5c2
honzakostejn <kostejn@me.com>
2025-12-28 11:09:22 +0100

    adding framework public ssh key

--- hosts/x86_64-linux/jellyfin-nixos-on-azure/default.nix
+++ hosts/x86_64-linux/jellyfin-nixos-on-azure/default.nix
@@ -22,7 +22,10 @@
     isNormalUser = true;
     home = "/home/honzakostejn";
     extraGroups = ["wheel"];
-    openssh.authorizedKeys.keys = [(builtins.readFile ~/.ssh/id_ed25519.pub)];
+    openssh.authorizedKeys.keys = [
+      # (builtins.readFile ~/.ssh/id_ed25519.pub)
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHQQyWy4iYbl9eifEfPJ7krH7p7jELm9TcSKBuDJHckH kostejn@me.com"
+    ];
     shell = pkgs.zsh;
   };
 
