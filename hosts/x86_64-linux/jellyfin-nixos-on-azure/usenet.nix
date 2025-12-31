21ebcebc883e3d27af9be96ee68a0858f72c8f8c
honzakostejn <kostejn@me.com>
2025-12-31 14:11:17 +0100

    usenet container tests

--- hosts/x86_64-linux/jellyfin-nixos-on-azure/usenet.nix
+++ hosts/x86_64-linux/jellyfin-nixos-on-azure/usenet.nix
@@ -18,47 +18,49 @@
 
       programs.zsh.enable = true;
       services.nzbget.enable = true;
+      networking.firewall.enable = true;
+      networking.firewall.allowedTCPPorts = [ 6789 ]; # NzbGet
 
-      networking.wg-quick.interfaces = {
-        wg0 = {
-          autostart = true;
-          address = [ 
-            "10.2.0.2/32"
-          ];
-          dns = [ "10.2.0.1" ];
-          privateKeyFile = "privatekeyhere";
-          peers = [
-            {
-              publicKey = "publickeyhere";
-              allowedIPs = [
-                "0.0.0.0/0"
-                "::/0"
-              ];
-              endpoint = "149.34.251.134:51820";
-            }
-          ];
-          postUp = ''iptables -I OUTPUT ! -o %i -m mark ! --mark $(wg show %i fwmark) -m addrtype ! --dst-type LOCAL -j REJECT && ip6tables -I OUTPUT ! -o %i -m mark ! --mark $(wg show %i fwmark) -m addrtype ! --dst-type LOCAL -j REJECT'';
-          preDown = ''iptables -D OUTPUT ! -o %i -m mark ! --mark $(wg show %i fwmark) -m addrtype ! --dst-type LOCAL -j REJECT && ip6tables -D OUTPUT ! -o %i -m mark ! --mark $(wg show %i fwmark) -m addrtype ! --dst-type LOCAL -j REJECT'';
-        };
-      };
+      # networking.wg-quick.interfaces = {
+      #   wg0 = {
+      #     autostart = true;
+      #     address = [ 
+      #       "10.2.0.2/32"
+      #     ];
+      #     dns = [ "10.2.0.1" ];
+      #     privateKeyFile = "privatekeyhere";
+      #     peers = [
+      #       {
+      #         publicKey = "publickeyhere";
+      #         allowedIPs = [
+      #           "0.0.0.0/0"
+      #           "::/0"
+      #         ];
+      #         endpoint = "149.34.251.134:51820";
+      #       }
+      #     ];
+      #     postUp = ''iptables -I OUTPUT ! -o %i -m mark ! --mark $(wg show %i fwmark) -m addrtype ! --dst-type LOCAL -j REJECT && ip6tables -I OUTPUT ! -o %i -m mark ! --mark $(wg show %i fwmark) -m addrtype ! --dst-type LOCAL -j REJECT'';
+      #     preDown = ''iptables -D OUTPUT ! -o %i -m mark ! --mark $(wg show %i fwmark) -m addrtype ! --dst-type LOCAL -j REJECT && ip6tables -D OUTPUT ! -o %i -m mark ! --mark $(wg show %i fwmark) -m addrtype ! --dst-type LOCAL -j REJECT'';
+      #   };
+      # };
 
-      # debug info
-      systemd.services.print-ip-after-boot = {
-        after = [ "network-online.target" ];
-        wantedBy = [ "multi-user.target" ];
-        serviceConfig = {
-          Type = "oneshot";
-          ExecStart = "curl -s https://api.ipify.org > /mnt/media/ip-after-boot.txt";
-        };
-      };
-      systemd.services.print-ip-after-60-seconds = {
-        after = [ "network-online.target" ];
-        wantedBy = [ "multi-user.target" ];
-        serviceConfig = {
-          Type = "oneshot";
-          ExecStart = "${pkgs.coreutils}/bin/sleep 60 && curl -s https://api.ipify.org > /mnt/media/ip-after-60-seconds.txt";
-        };
-      };
+      # # debug info
+      # systemd.services.print-ip-after-boot = {
+      #   after = [ "network-online.target" ];
+      #   wantedBy = [ "multi-user.target" ];
+      #   serviceConfig = {
+      #     Type = "oneshot";
+      #     ExecStart = "curl -s https://api.ipify.org > /mnt/media/ip-after-boot.txt";
+      #   };
+      # };
+      # systemd.services.print-ip-after-60-seconds = {
+      #   after = [ "network-online.target" ];
+      #   wantedBy = [ "multi-user.target" ];
+      #   serviceConfig = {
+      #     Type = "oneshot";
+      #     ExecStart = "${pkgs.coreutils}/bin/sleep 60 && curl -s https://api.ipify.org > /mnt/media/ip-after-60-seconds.txt";
+      #   };
+      # };
     };
 
     forwardPorts = [
