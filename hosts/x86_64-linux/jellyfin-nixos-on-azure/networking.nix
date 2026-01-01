19655fceef30274949333ea561ad518a687abba5
honzakostejn <kostejn@me.com>
2026-01-01 13:51:17 +0100

    change in the address

--- hosts/x86_64-linux/jellyfin-nixos-on-azure/networking.nix
+++ hosts/x86_64-linux/jellyfin-nixos-on-azure/networking.nix
@@ -11,7 +11,7 @@
         reverse_proxy 127.0.0.1:8096
       '';
       "nzbget.kosh.boo".extraConfig = ''
-        reverse_proxy http://192.168.100.11:6789
+        reverse_proxy 192.168.100.11:6789
       '';
     };
   };  
