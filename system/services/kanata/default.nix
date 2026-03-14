2a43d404e8e23672b10bb968f6e724d1334928e4
honzakostejn <kostejn@me.com>
2026-03-14 22:04:11 +0100

    dell keyboard support

--- system/services/kanata/default.nix
+++ system/services/kanata/default.nix
@@ -41,6 +41,11 @@
           devices = [ "/dev/input/by-id/usb-LiteOn_Lenovo_Calliope_USB_Keyboard-event-kbd" ];
           config = builtins.readFile (./. + "/kanata.kbd");
         };
+
+        dell = {
+          devices = [ "/dev/input/by-id/usb-Dell_Dell_Wired_Multimedia_Keyboard-event-kbd" ];
+          config = builtins.readFile (./. + "/kanata.kbd");
+        };
       };
     };
   };
