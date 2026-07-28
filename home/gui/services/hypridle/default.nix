bc28e93d457e82fe700f03a9a1b1d3c596bd6491
honzakostejn <kostejn@me.com>
2026-07-28 21:31:43 +0200

    avoiding locking the device when playing the music

--- home/gui/services/hypridle/default.nix
+++ home/gui/services/hypridle/default.nix
@@ -1,17 +1,19 @@
 { lib
-, inputs
+, config
 , pkgs
+, inputs
 , ...
 }:
 let
   minuteInSeconds = 60;
 
   suspendScript = pkgs.writeShellScript "suspend-script" ''
+    #!/bin/bash
     isCharging=$(cat /sys/class/power_supply/ACAD/online)
-    if [ $isCharging == 0 ]; then
-      isSomethingPlaying=$(${lib.getExe pkgs.playerctl} -a status | ${lib.getExe pkgs.ripgrep} Playing -q)
+    if [ $isCharging -eq 0 ]; then
+      isSomethingPlaying=$(${lib.getExe pkgs.playerctl} -a status) # | ${lib.getExe pkgs.ripgrep} Playing -q)
       # only suspend if nothing is playing
-      if [ $isSomethingPlaying == 1 ]; then
+      if [ "$isSomethingPlaying" -ne "Playing" ]; then
         ${pkgs.systemd}/bin/systemctl suspend
       fi
     fi
@@ -28,35 +30,35 @@ in
 {
   # screen idle
   services.hypridle = {
-      enable = true;
+    enable = true;
 
-      package = inputs.hypridle.packages.${pkgs.stdenv.hostPlatform.system}.hypridle;
+    package = inputs.hypridle.packages.${pkgs.stdenv.hostPlatform.system}.hypridle;
 
-      settings = {
-        general = {
-          lock_cmd = "${custom-hyprlock-script}/bin/custom-hyprlock-script";
-          before_sleep_cmd = "${pkgs.systemd}/bin/loginctl lock-session";
-        };
-
-        listener = [
-          # {
-          #   timeout = timeout - 10;
-          #   # save the current brightness and dim the screen over a period of
-          #   # 1 second
-          #   on-timeout = "${brillo} -O; ${brillo} -u 1000000 -S 10";
-          #   # brighten the screen over a period of 500ms to the saved value
-          #   on-resume = "${brillo} -I -u 500000";
-          # }
-          {
-            inherit timeout;
-            on-timeout = "hyprctl dispatch dpms off";
-            on-resume = "hyprctl dispatch dpms on";
-          }
-          {
-            timeout = timeout + 10;
-            on-timeout = suspendScript.outPath;
-          }
-        ];
+    settings = {
+      general = {
+        lock_cmd = "${custom-hyprlock-script}/bin/custom-hyprlock-script";
+        before_sleep_cmd = "${pkgs.systemd}/bin/loginctl lock-session";
       };
+
+      listener = [
+        # {
+        #   timeout = timeout - 10;
+        #   # save the current brightness and dim the screen over a period of
+        #   # 1 second
+        #   on-timeout = "${brillo} -O; ${brillo} -u 1000000 -S 10";
+        #   # brighten the screen over a period of 500ms to the saved value
+        #   on-resume = "${brillo} -I -u 500000";
+        # }
+        {
+          inherit timeout;
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on";
+        }
+        {
+          timeout = timeout + 10;
+          on-timeout = suspendScript.outPath;
+        }
+      ];
+    };
   };
 }
