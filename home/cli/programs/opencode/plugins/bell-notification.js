e15b946f1b7e78e921c712dc9e880e9a0364c3d1
honzakostejn <kostejn@me.com>
2026-08-10 21:04:39 +0200

    change event for bell notification

--- home/cli/programs/opencode/plugins/bell-notification.js
+++ home/cli/programs/opencode/plugins/bell-notification.js
@@ -22,8 +22,9 @@ function ringBell(now = Date.now()) {
 export const NotificationPlugin = async ({ project, client, $, directory, worktree }) => {
   return {
     event: async ({ event }) => {
-      // Ring the terminal bell on session completion
-      if (event.type === "session.idle") {
+      // ring the terminal bell when requiring attention
+      // https://github.com/anomalyco/opencode/blob/b9f3b382fcfd82b57103b29b77572f112ce9e1e5/packages/web/src/content/docs/plugins.mdx#events
+      if (event.type === "session.status" ) {
         // await ringBell()
         process.stdout.write("\x07")
       }
